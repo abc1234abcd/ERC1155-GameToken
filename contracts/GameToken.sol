a0b442628785db246dc8a82d52ed0e924ecf4458
myusername <bingxu1126@gmail.com>
2022-10-05 16:27:56 +0100

    main

--- contracts/GameToken.sol
+++ contracts/GameToken.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-
-
 
 contract GameToken is ERC1155 {
     
@@ -14,6 +12,8 @@ contract GameToken is ERC1155 {
     constructor() ERC1155("") {
         name ="IKA";
         symbol = "IKA";
-        _mint(msg.sender, 1, 100, "https://ipfs.io/ipfs/bafyreidck3w2txmrrc7dhpbs56pjgo5y4chrokjg24k6dgzhmtjqkfmpia/metadata.json");
+        _mint(msg.sender, 1, 100, "https://ipfs.io/ipfs/bafybeib7omnw4nj2tod3g2ofxir7uie4ys6fg7q6v7s4v45g6h5r4d2fje/peony.json");
+        _mint(msg.sender, 2, 100, "https://ipfs.io/ipfs/bafybeib7omnw4nj2tod3g2ofxir7uie4ys6fg7q6v7s4v45g6h5r4d2fje/rose.json");
+        _mint(msg.sender, 3, 100, "https://ipfs.io/ipfs/bafybeib7omnw4nj2tod3g2ofxir7uie4ys6fg7q6v7s4v45g6h5r4d2fje/tulip.json");
     }
 }
