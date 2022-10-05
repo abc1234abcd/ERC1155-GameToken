ec6012fb3f22161ec37140b8909f737814faec49
myusername <bingxu1126@gmail.com>
2022-10-05 18:02:49 +0100

    2nd time commit

--- contracts/GameToken.sol
+++ contracts/GameToken.sol
@@ -3,17 +3,28 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract GameToken is ERC1155 {
     
     string public name;
     string public symbol;
 
-    constructor() ERC1155("") {
+    uint public constant rose=0;
+    uint public constant peony=1;
+    uint public constant tulip=2;
+
+    mapping (uint256 => string) private _uris;
+
+    constructor() ERC1155("https://bafybeiadwlbaowbmpqllgxsiy6kta5wn7d3uzznpc2igxxp6rrrac2gud4.ipfs.nftstorage.link/{id}.json") {
         name ="IKA";
         symbol = "IKA";
-        _mint(msg.sender, 1, 100, "https://ipfs.io/ipfs/bafybeib7omnw4nj2tod3g2ofxir7uie4ys6fg7q6v7s4v45g6h5r4d2fje/peony.json");
-        _mint(msg.sender, 2, 100, "https://ipfs.io/ipfs/bafybeib7omnw4nj2tod3g2ofxir7uie4ys6fg7q6v7s4v45g6h5r4d2fje/rose.json");
-        _mint(msg.sender, 3, 100, "https://ipfs.io/ipfs/bafybeib7omnw4nj2tod3g2ofxir7uie4ys6fg7q6v7s4v45g6h5r4d2fje/tulip.json");
+        _mint(msg.sender, rose, 100, "");
+        _mint(msg.sender, peony, 100, "");
+        _mint(msg.sender, tulip, 100, "");
     }
+
+        function uri(uint256 tokenId) override public view returns (string memory) {
+        return(_uris[tokenId]);
+    }   
 }
